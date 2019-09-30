e2f06ade3b468f34e2adf974b0b64df684eb2207
github username <github email address>
2019-09-30 15:45:41 -0700

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,39 @@
-class Deck
-
-end
-
+require 'pry'
 class Card
-
+    attr_reader :rank, :suit
+    def initialize(rank, suit)
+        @rank = rank
+        @suit = suit
+    end
 end
+
+class Deck
+    attr_reader :cards
+    def initialize
+
+        
+        @cards = []
+        
+        ranks = ["Hearts", "Clubs", "Diamonds", "Spades"]
+        suits = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        
+        ranks.each { |rank|
+            suits.each { |suit|
+                newcard = Card.new(rank, suit)
+                @cards << newcard
+            }
+        }
+    end
+
+    def choose_card
+        @cards.shuffle!  # bang is changing array
+        chosen_card = @cards.pop
+        puts chosen_card
+    end
+    binding.pry
+end
+
+
+
+
+
